207e32cb06ca8f5727a0471fef8097f28ffe55d3
Richard Millet <milletdesigns@me.com>
2012-08-04 18:42:44 -0700

    Experimenting with application state transitions.

--- Homepwner/HomepwnerAppDelegate.m
+++ Homepwner/HomepwnerAppDelegate.m
@@ -16,6 +16,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    NSLog(@"Application started at %@", [NSDate date]);
+
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
 	// Override point for customization after application launch.
@@ -38,6 +41,7 @@
 {
 	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
 	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -51,21 +55,26 @@
     } else {
         NSLog(@"Failed to save items.");
     }
+    
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
 	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
 	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
 	// Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    NSLog(@"%@", NSStringFromSelector(_cmd));
 }
 
 @end
