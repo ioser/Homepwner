b03fab1eb8788836865a36b7e1a7c16599bef442
Richard Millet <milletdesigns@me.com>
2012-08-10 23:26:23 -0700

    Starting chapter 15.

---
+++ Homepwner/HomepwnerItemCell.h
@@ -0,0 +1,13 @@
+//
+//  HomepwnerItemCell.h
+//  Homepwner
+//
+//  Created by Richard Millet on 8/10/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerItemCell : UITableViewCell
+
+@end
---
+++ Homepwner/HomepwnerItemCell.m
@@ -0,0 +1,29 @@
+//
+//  HomepwnerItemCell.m
+//  Homepwner
+//
+//  Created by Richard Millet on 8/10/12.
+//
+//
+
+#import "HomepwnerItemCell.h"
+
+@implementation HomepwnerItemCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
